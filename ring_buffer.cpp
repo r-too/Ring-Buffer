0a7dca9b011789a69d98eefbafa3bcfbbba01cd5
rtoo <android.super.paranoid@gmail.com>
2019-12-03 20:21:21 +0000

    Added new member functions in RList

--- ring_buffer.cpp
+++ ring_buffer.cpp
@@ -8,31 +8,44 @@ using namespace std;
 
 class Node {
   public:
-  int data = 0;
+  int data;
   Node *prev, *next;
 };
 
 class RList {
+  void rdcopy(const RList& other);
   public:
   string name;
   Node head, tail;
   RList(string argname) {
     head.next = head.prev = &tail;
     tail.next = tail.prev = &head;
+    head.data = tail.data = 0;
     name = argname;
   };
   ~RList();
-  void add(int ldata);
-  void pop(int ldata, Node *pnode);
-  void push(int ldata, Node *pnode);
-  void del(int ldata);
-  void print(bool dir);
-  void dcopy(const RList& other);
+  void radd(int ldata);
+  void rpop_h(int ldata, Node *pnode);
+  void rpop_t(int ldata, Node *pnode);
+  void rpush_h(int ldata, Node *pnode);
+  void rpush_t(int ldata, Node *pnode);
+  void rinsert_at(int ldata, Node *pnode);
+  void rmerge(int ldata, Node *pnode);
+  void rsort(Node *pnode);
+  void rsplice();
+  void rextract(int ldata, Node *pnode);
+  void rdel(int ldata);
+  void rprint(bool dir);
+  void rreverse(Node *pnode);
+  void rshift_h(int ldata, Node *pnode);
+  void rshift_t(int ldata, Node *head_node);
+  void rresize(int num, Node *head_node);
+  void runique(Node *head_node);
   RList(const RList& other);
   RList& operator=(const RList& other);
 };
 
-void RList::dcopy(const RList& other) {
+void RList::rdcopy(const RList& other) {
   Node *scurr = other.tail.next;
   bool head_copied = false;
 
@@ -40,20 +53,20 @@ void RList::dcopy(const RList& other) {
     if (scurr == &other.head && head_copied) {
       return;
     } else if (scurr == &other.head && !head_copied) {
-      head_copied = true, this->add(scurr->data), scurr = scurr->next;
+      head_copied = true, this->radd(scurr->data), scurr = scurr->next;
     } else {
-      this->add(scurr->data), scurr = scurr->next;
+      this->radd(scurr->data), scurr = scurr->next;
     }
   }
 }
 
 RList::RList(const RList& other) {
-  dcopy(other);
+  rdcopy(other);
 }
 
 RList& RList::operator=(const RList& other) {
   if (this == &other) return *this;
-  dcopy(other);
+  rdcopy(other);
   return *this;
 }
 
@@ -64,7 +77,7 @@ RList::~RList() {
   cout << "[FREE] (" << name << ") elements" << endl;
 }
 
-void RList::print(bool dir) {
+void RList::rprint(bool dir) {
   if (dir == FORWARD) {
     Node *tmp = &head;
     cout << "[FORWARD] (" << name << ") ";
@@ -90,7 +103,7 @@ void RList::print(bool dir) {
   }
 }
 
-void RList::add(int ldata) {
+void RList::radd(int ldata) {
   if (head.data == 0) {
     head.data = ldata;
     return;
@@ -109,15 +122,18 @@ void RList::add(int ldata) {
 }
 
 class RStack: public RList {
+  ~RStack();
 };
 
 class RQueue: public RList {
+  ~RQueue();
 };
 
 class RBuffer: public RList {
+  ~RBuffer();
 };
 
-void RList::del(int ldata) {
+void RList::rdel(int ldata) {
   Node *tmp = &head;
 
   while(tmp) {
@@ -142,22 +158,22 @@ void RList::del(int ldata) {
 int main()
 {
   RList nlist("nlist"), clist("clist"), plist("plist"), blist("blist");
-  nlist.add(4); // goes into head
-  nlist.add(5); // the rest go in between
-  nlist.add(6);
-  nlist.add(7);
-  nlist.add(8);
-  nlist.add(9); // goes into tail
+  nlist.radd(4); // goes into head
+  nlist.radd(5); // the rest go in between
+  nlist.radd(6);
+  nlist.radd(7);
+  nlist.radd(8);
+  nlist.radd(9); // goes into tail
 
-  nlist.print(FORWARD);
-  nlist.print(REWIND);
+  nlist.rprint(FORWARD);
+  nlist.rprint(REWIND);
 
   clist = nlist;
-  clist.print(FORWARD);
+  clist.rprint(FORWARD);
   plist = clist;
-  plist.print(FORWARD);
+  plist.rprint(FORWARD);
   blist = plist;
-  blist.print(FORWARD);
+  blist.rprint(FORWARD);
 
   cout << "[END]" << endl;
   return 0;
